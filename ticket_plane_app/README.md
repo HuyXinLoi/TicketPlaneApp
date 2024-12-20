e958633865bf30fd8efd015f89209e16c2dc4ee8
HuyXinLoi <huyhodinh0909@gmail.com>
2024-12-20 20:32:08 +0700

    first_fe_app

---
+++ ticket_plane_app/README.md
@@ -0,0 +1,3 @@
+# ticket_plane_app
+
+A new Flutter project.
---
+++ ticket_plane_app/android/app/src/main/kotlin/com/example/ticket_plane_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ticket_plane_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
